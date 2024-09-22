1bf27e8fc2a9b0d5bbcac3ea1806436ad3e25d9a
Ball Pornpavee <pornpavee.ji@gmail.com>
2024-09-23 00:52:46 +0700

    init finger page

--- src/pages/Finger.tsx
+++ src/pages/Finger.tsx
@@ -1,16 +1,32 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import MainButton from 'components/MainButton'
+import tw from 'utilities/tw'
+
+const classes = {
+  container: tw(`flex flex-col gap-6 md:gap-8`),
+  subContainer: tw(`flex flex-col gap-4 md:gap-6 lg:gap-8`),
+  contentSection: tw(`w-full px-8 pt-8 pb-2 lg:px-12 lg:pt-10 lg:pb-4 flex flex-col
+    gap-3 lg:gap-5 border rounded-xl shadow-custom-1`),
+  title: tw(`text-center text-secondary text-sm md:text-lg lg:text-2xl`),
+  fingerVD: tw(`self-center`),
+  nextBtn: tw(`w-full max-w-none md:min-w-[100%] lg:min-w-[100%]`),
+}
 
 const Finger = () => {
-  const navigate = useNavigate()
-
-  const handleBack = () => {
-    navigate('/')
-  }
-
   return (
-    <div>
-      <button onClick={handleBack}>Back</button>
+    <div className={classes.container}>
+      <MainButton text="Back" />
+
+      <div className={classes.subContainer}>
+        <div className={classes.contentSection}>
+          <div className={classes.title}>จุดไหนที่คุณปวดนิ้วมากที่สุด ?</div>
+          <div className={classes.fingerVD}>
+            {/* <Diagram data={fingerData} baseImage={defaultFinger} /> */}
+          </div>
+        </div>
+
+        <MainButton href="finger" text="ต่อไป" styles={classes.nextBtn} />
+      </div>
     </div>
   )
 }
