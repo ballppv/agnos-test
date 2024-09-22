51472e54ea000e7f92f16462128b7f7381e61890
Ball Pornpavee <pornpavee.ji@gmail.com>
2024-09-22 23:13:28 +0700

    add font

--- src/pages/Abs.tsx
+++ src/pages/Abs.tsx
@@ -4,10 +4,10 @@ import tw from 'utilities/tw'
 import VoronoiDiagram from 'components/Diagram'
 import { absData } from 'utilities/mockUpData'
 import defaultAbs from 'assets/images/abs/default-abs.png'
-// import allOverPart from '../assets/images/abs/all-over-highlight.png'
 
 const classes = {
-  container: tw(`flex flex-col gap-5`),
+  container: tw(`flex flex-col gap-12`),
+  subContainer: tw(`flex flex-col gap-5`),
   absVD: tw(`self-center`),
 }
 
@@ -15,9 +15,10 @@ const Abs = () => {
   return (
     <div className={classes.container}>
       <MainButton text="Back" styles="max-w-[200px]" />
-      {/* <ClickableBaseImage baseImage={baseImage} partImages={partImages} detailImages={textImages} /> */}
-      <div className={classes.absVD}>
-        <VoronoiDiagram data={absData} baseImage={defaultAbs} />
+      <div className={classes.subContainer}>
+        <div className={classes.absVD}>
+          <VoronoiDiagram data={absData} baseImage={defaultAbs} />
+        </div>
       </div>
     </div>
   )
